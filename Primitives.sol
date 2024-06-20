21029276449743aededb73867c1e1a46d1a93bbe
opmklinh <61728239+opmklinh@users.noreply.github.com>
2024-06-20 17:46:41 +0700

    Update Primitives.sol

--- Primitives.sol
+++ Primitives.sol
@@ -3,7 +3,7 @@
 
 pragma solidity >=0.8.0;
 
-import {BranchlessMath} from "./utils/BranchlessMath.sol";
+import {BranchlessMath} from "BranchlessMath.sol";
 
 /**
  * @dev GmpSender is the sender of a GMP message
