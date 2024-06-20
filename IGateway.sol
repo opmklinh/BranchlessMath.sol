323962ea3983c892a487d814484e12e2d358c834
opmklinh <61728239+opmklinh@users.noreply.github.com>
2024-06-20 17:47:19 +0700

    Update IGateway.sol

--- IGateway.sol
+++ IGateway.sol
@@ -3,7 +3,7 @@
 
 pragma solidity >=0.8.0;
 
-import {GmpSender} from "../Primitives.sol";
+import {GmpSender} from "Primitives.sol";
 
 /**
  * @dev Required interface of an Gateway compliant contract
